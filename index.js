2837c303a6341f534e26a3b98ccd413b52908d1e
SValls2023 <54817897+SValls2023@users.noreply.github.com>
2023-11-24 21:59:32 -0800

    Feat: Create slider input to change frid size

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // Grid Container 
 const gridContainer = document.querySelector('#grid-container');
-const changeSize = document.querySelector('#size-container');
+const slider = document.querySelector('#slider');
+const slideValue = document.querySelector('#value');
 
-changeSize.addEventListener('click', (e) => {
-    if (e.target.tagName === 'BUTTON') {
-        makeGrid(e.target.value);
-    }
-})
+slider.oninput = function () {
+    makeGrid(this.value);
+    slideValue.textContent = `${this.value} x ${this.value}`;
+};
+
+// changeSize.addEventListener('click', (e) => {
+//     if (e.target.tagName === 'BUTTON') {
+//         makeGrid(e.target.value);
+//     }
+// })
 
 // Note for later: 
 // 8 * 8 = 60px;
@@ -14,22 +20,15 @@ changeSize.addEventListener('click', (e) => {
 // 24 * 24 = 20px;
 function makeGrid(squaresBy = 16) {
     gridContainer.replaceChildren();
+    let squarePixels = 480/parseFloat(squaresBy);
     for (let i = 0; i < squaresBy; i++) {
         let row = document.createElement('div');
         //console.log(row)
         row.classList.add("row");
         for (let j = 0; j < squaresBy; j++) {
             const square = document.createElement('div');
-            if (squaresBy == 16) {
-                square.style.height = "30px";
-                square.style.width = "30px";
-            } else if (squaresBy == 8) {
-                square.style.height = "60px";
-                square.style.width = "60px";
-            } else if (squaresBy == 24) {
-                square.style.height = "20px";
-                square.style.width = "20px";
-            }
+            square.style.height = `${squarePixels}px`;
+            square.style.width = `${squarePixels}px`;
             row.appendChild(square);
             //console.log(square);
         }
